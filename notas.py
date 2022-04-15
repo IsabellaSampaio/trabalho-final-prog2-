40a7d41c5e409ca541857f8b3d23d59d825150e7
Isabella Sampaio <71940799+IsabellaSampaio@users.noreply.github.com>
2022-04-15 20:18:28 -0300

    Update notas.py

--- notas.py
+++ notas.py
@@ -1,32 +1,28 @@
 #Trabalho final de Programação II
 #Aluna: Isabella Sampaio
-#Matrícula: 20211BSI0208  
 #--------------//--------------
 
 import os 
 import pickle
 
-#Função para limpar a tela 
 def limpaTela():
 	if os.name=="nt":
 		os.system("cls")
 	else:
 		os.system("clear")
 
-#--------------//--------------
 
-#Função que salvará as informações no arquivo binário
+
 def salvarInfos(a,n):
 	with open ("entrada.bin","wb+") as f:
 		pickle.dump(a,f)
 		pickle.dump(n,f)
 
-#--------------//--------------
 
-#Função 1 de ordenação: Função responsável por indicar as funções partição e quick sort quais os criterios de ordenação. 
+
+
 def anterior(x,y, alunos):
 
-	#Definindo variáveis locais
 
 	x1,x2,x3,x4,x5,x6 = x
 	y1,y2,y3,y4,y5,y6 = y
@@ -34,31 +30,28 @@ def anterior(x,y, alunos):
 	somax = x2 + x3 + x4 + x5
 	somay = y2 + y3 + y4 + y5
 
-	#Primeiro critério: Soma total de pontos
+	
 	if somax > somay: return True
 	if somax < somay : return False
 
-	#Segundo critério: Nota 2 
+	 
 	if x3 > y3: return True
 	if x3 < y3: return False 
 
-	#Terceiro critério: Tempo de execução  
+	
 	if x6 < y6: return True
 	if x6 > y6:return False
 
-	#Colaca os alunos em ordem alfabética
+	
 	if alunos[x1] < alunos[y1]: return True
 	return False
-
-#--------------//--------------
-
-#Função 2 de ordenação: Função responsável por ordenar i e j (menores e maiores) de acordo com a função anterior      
+  
 def particao(l,inf,sup, alunos):
 	pivot = l[inf]
 	i = inf+1
 	j = sup
 
-	#Andando com i e com j de acordo com o pivot e sua posição 
+	 
 	while i <= j:
 		while i <= j and anterior(l[i], pivot, alunos):
 			i+=1
@@ -66,22 +59,19 @@ def particao(l,inf,sup, alunos):
 			j -= 1
 		if i < j: l[i], l[j] = l[j], l[i]
 	
-	#Troca de posição final, para colocar o pivot no lugar certo  		
+	 		
 	l[inf],l[j] = l[j],l[inf]
 	return j 
 
-#--------------//--------------
 
-#Função 3 de ordenação: Função responsável por de fato ordenar de acordo com as outras duas   
 def quickSort(l,inf,sup, alunos):
 	if inf < sup:
 		pos = particao(l,inf,sup, alunos)
 		quickSort(l,inf,pos-1, alunos)
 		quickSort(l,pos+1,sup, alunos)
 
-#--------------//--------------
 
-#Função para inserir alunos
+
 def addAluno(alunos):
 	matricula = int(input("INSIRA A MATRÍCULA DO ALUNO: "))
 	nome = input("INSIRA O NOME DO ALUNO: ")
@@ -93,9 +83,8 @@ def addAluno(alunos):
 	
 		print("ALUNO ADICIONADO","\n")
 
-#--------------//--------------
 
-#Função para inserir as notas
+
 def addNotas(notas):
 	l = []
 	matricula = int(input("INSIRA O NUMERO DA MATRÍCULA: "))
@@ -112,19 +101,17 @@ def addNotas(notas):
 	print(notas)
 	print("NOTAS ADICIONADAS","\n")
 
-#--------------//--------------
 
-#Função que chama o quickSort e faz o acréscimo de +2 pontos  
+  
 def soma(l,alunos):
 
-	#definindo variavis locais
+	
 	n = []
 	n2 = []
 	i = 0
 	sup = (len(l)-1)
 	inf = (len(l)-1-sup)
 	
-	#Chamando a ordenação uma unica vez e logo depois fazendo o acrescimo dos +2 pontos 
 	quickSort(l,inf,sup,alunos)
 	for i in range (len(l)):
 
@@ -140,11 +127,7 @@ def soma(l,alunos):
 		else:
 			print(alunos[pos[i]],"", n[i])
 
-	#nota: n e n2 são apenas listas auxiliares 
 
-#--------------//--------------
-
-#Função principal 
 def main():
 
 	opcaoUsuario = str
@@ -156,7 +139,7 @@ def main():
 DIGITE 1 PARA VISUALIZAR AS NOTAS.
 ->: '''
 	
-	#Lendo o que tem dentro do arquivo  
+	
 	if os.path.isfile("entrada.bin"):
 		with open("entrada.bin","rb") as f:
 			alunos = pickle.load(f)
@@ -173,22 +156,10 @@ DIGITE 1 PARA VISUALIZAR AS NOTAS.
 		print("OPÇÃO INVALIDA. TENTE NOVAMENTE.") 
 		opcaoUsuario = input(menu)		
 
-	#while opcaoUsuario != "3":
-		#if opcaoUsuario == "1":
-			#addAluno(alunos)
-		#elif opcaoUsuario == "2":
-			#addNotas(notas)
-		#menu='''DESEJA ADICIONAR MAIS NOTAS E/OU ALUNOS? 
-#DIGITE 1 PARA ADICIONAR + ALUNOS, 2 PARA ADICIONAR + NOTAS OU 3 CASO QUEIRA SAIR
-#: '''	
-		#opcaoUsuario= input(menu)
-	#limpaTela()
-
-	#Salvando no arquivo e printando a lista ordenada de alunos e seus correspondentes pontos   	
+	
 	salvarInfos(alunos,notas)
 		
 
 
-#Fim da função principal e do programa
 if __name__=='__main__':
     main()
